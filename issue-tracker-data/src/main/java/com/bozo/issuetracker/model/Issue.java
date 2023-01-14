15048b40470554c6a82bc48e8109f98b39472612
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-01-14 18:47:30 +0100

    add orm to models. #18

--- issue-tracker-data/src/main/java/com/bozo/issuetracker/model/Issue.java
+++ issue-tracker-data/src/main/java/com/bozo/issuetracker/model/Issue.java
@@ -1,10 +1,20 @@
 package com.bozo.issuetracker.model;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
+@Entity
+@Table(name = "issue")
 public class Issue extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "app_user_id")
     private User issueCreator;
-    private List<User> issueObserver;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "issue")
+    private List<IssueComment> comments;
 }
--- issue-tracker-data/src/main/java/com/bozo/issuetracker/model/IssueComment.java
+++ issue-tracker-data/src/main/java/com/bozo/issuetracker/model/IssueComment.java
@@ -1,8 +1,19 @@
 package com.bozo.issuetracker.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "comment")
 public class IssueComment extends BaseEntity {
 
+    @Column(name = "comment")
     private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "app_user_id")
     private User commentCreator;
+
+    @ManyToOne
+    @JoinColumn(name = "issue_id")
     private Issue issue;
 }
