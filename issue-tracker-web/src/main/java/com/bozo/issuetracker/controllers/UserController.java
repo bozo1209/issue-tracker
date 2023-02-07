0cecf2b0e59c5f03f812813496c2d1d3d72b64ad
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-02-07 22:52:49 +0100

    add edit user. #105

--- issue-tracker-web/src/main/java/com/bozo/issuetracker/controllers/UserController.java
+++ issue-tracker-web/src/main/java/com/bozo/issuetracker/controllers/UserController.java
@@ -49,11 +49,18 @@ public class UserController {
 
     @GetMapping("/{id}/edit")
     public String editUser(@PathVariable Long id, Model model){
-        return null;
+        model.addAttribute("user", userService.findById(id));
+        return HTMLPaths.ADD_EDIT_USER.getPath();
     }
 
     @PostMapping("/{id}/edit")
     public String processEditingUser(@Valid User user, @PathVariable Long id, BindingResult result){
-        return null;
+        if (result.hasErrors()){
+            return HTMLPaths.ADD_EDIT_USER.getPath();
+        }
+        User userById = userService.findById(id);
+        userById.setUserName(user.getUserName());
+        User savedUser = userService.save(userById);
+        return "redirect:/user/" + savedUser.getId();
     }
 }
