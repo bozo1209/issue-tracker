f5a90215eb685bb728f5b7df0ac9b712cbe4a242
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-03-11 22:43:13 +0100

    fix: No AuthenticationProvider found. #214

--- issue-tracker-security/src/main/java/com/bozo/issuetracker/config/ApplicationSecurityConfig.java
+++ issue-tracker-security/src/main/java/com/bozo/issuetracker/config/ApplicationSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.bozo.issuetracker.config;
 
+import com.bozo.issuetracker.details.service.ApplicationUserDetailsService;
 import com.bozo.issuetracker.enums.UserRoles;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +24,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ApplicationSecurityConfig {
 
     private final PasswordConfig passwordConfig;
+    private final ApplicationUserDetailsService applicationUserDetailsService;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
@@ -29,24 +33,33 @@ public class ApplicationSecurityConfig {
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated());
         http.formLogin();
+        http.authenticationProvider(authenticationProvider());
         http.csrf().ignoringRequestMatchers(PathRequest.toH2Console()).and().headers().frameOptions().sameOrigin(); // allow opening h2-console, fo dev purpose only
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user1 = User.builder()
-                .username("user1")
-                .password(passwordConfig.passwordEncoder().encode("user1"))
-                .roles(UserRoles.ADMIN.name())
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("user2")
-                .password(passwordConfig.passwordEncoder().encode("user2"))
-                .roles(UserRoles.USER.name())
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(applicationUserDetailsService);
+        provider.setPasswordEncoder(passwordConfig.passwordEncoder());
+        return provider;
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user1 = User.builder()
+//                .username("user1")
+//                .password(passwordConfig.passwordEncoder().encode("user1"))
+//                .roles(UserRoles.ADMIN.name())
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("user2")
+//                .password(passwordConfig.passwordEncoder().encode("user2"))
+//                .roles(UserRoles.USER.name())
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
 }
