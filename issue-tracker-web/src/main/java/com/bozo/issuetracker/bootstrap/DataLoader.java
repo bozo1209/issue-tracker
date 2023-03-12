8b81148cf508a8859324ee33541b4d3560920f91
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-03-12 14:28:51 +0100

    code clean up, delete comments and unused baseit class. #218

--- issue-tracker-web/src/main/java/com/bozo/issuetracker/bootstrap/DataLoader.java
+++ issue-tracker-web/src/main/java/com/bozo/issuetracker/bootstrap/DataLoader.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Set;
 
 @Component
--- issue-tracker-web/src/test/java/com/bozo/issuetracker/controllers/security/IndexControllerSecurityTest.java
+++ issue-tracker-web/src/test/java/com/bozo/issuetracker/controllers/security/IndexControllerSecurityTest.java
@@ -1,17 +1,10 @@
 package com.bozo.issuetracker.controllers.security;
 
-import com.bozo.issuetracker.config.ApplicationSecurityConfig;
-import com.bozo.issuetracker.config.PasswordConfig;
 import com.bozo.issuetracker.controllers.IndexController;
 import com.bozo.issuetracker.controllers.security.config.ApplicationSecurityTestConfig;
-import com.bozo.issuetracker.details.service.ApplicationUserDetailsService;
-import com.bozo.issuetracker.repository.UserRepository;
-import com.bozo.issuetracker.service.UserService;
-import com.bozo.issuetracker.service.springdatajpa.UserSDJpaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,16 +12,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 
-//@Import({ApplicationSecurityConfig.class, PasswordConfig.class, ApplicationUserDetailsService.class, UserSDJpaService.class, UserRepository.class})
-//@WebMvcTest(controllers = IndexController.class)
-//@SpringBootTest
 @Import(ApplicationSecurityTestConfig.class)
 @WebMvcTest(controllers = IndexController.class)
-class IndexControllerSecurityTest
-//        extends BaseIT
-{
+class IndexControllerSecurityTest {
 
     @Autowired
     private MockMvc mockMvc;
