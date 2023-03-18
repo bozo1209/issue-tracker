ead87fe3e8f685f73dade35828b3604b194d5ef4
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-03-18 21:06:24 +0100

    set authorization on methods in issuecomment controller #183 #201

--- issue-tracker-web/src/main/java/com/bozo/issuetracker/controllers/IssueCommentController.java
+++ issue-tracker-web/src/main/java/com/bozo/issuetracker/controllers/IssueCommentController.java
@@ -1,6 +1,7 @@
 package com.bozo.issuetracker.controllers;
 
 import com.bozo.issuetracker.annotation.PreAuthorizeRoleAdmin;
+import com.bozo.issuetracker.annotation.PreAuthorizeRoleAdminOrRoleUser;
 import com.bozo.issuetracker.model.Issue;
 import com.bozo.issuetracker.model.IssueComment;
 import com.bozo.issuetracker.service.IssueCommentService;
@@ -23,6 +24,7 @@ public class IssueCommentController {
     private final IssueCommentService issueCommentService;
     private final IssueService issueService;
 // admin + user
+    @PreAuthorizeRoleAdminOrRoleUser
     @PostMapping("/new")
     public String processAddingComment(@Valid IssueComment comment, @PathVariable Long issueId, BindingResult result){
         Issue issueById = issueService.findById(issueId);
@@ -32,6 +34,7 @@ public class IssueCommentController {
         return "redirect:/issue/" + issueId;
     }
     // admin + user
+    @PreAuthorizeRoleAdminOrRoleUser
     @PostMapping("/{commentId}/edit")
     public String processEditingComment(@Valid IssueComment comment, @PathVariable Long issueId, @PathVariable Long commentId, BindingResult result){
         Issue issueById = issueService.findById(issueId);
@@ -42,6 +45,7 @@ public class IssueCommentController {
         return "redirect:/issue/" + issueId;
     }
     // admin + user
+    @PreAuthorizeRoleAdminOrRoleUser
     @GetMapping("/{commentId}/delete")
     public String deleteComment(@PathVariable Long commentId, @PathVariable Long issueId){
         issueService.findById(issueId).getComments().remove(issueCommentService.findById(commentId));
