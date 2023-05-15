7be65b1d334b7c53300396a9d996f21481e7c164
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-05-15 23:03:16 +0200

    update data loader  #259

--- issue-tracker-web/src/main/java/com/bozo/issuetracker/bootstrap/DataLoader.java
+++ issue-tracker-web/src/main/java/com/bozo/issuetracker/bootstrap/DataLoader.java
@@ -39,8 +39,11 @@ public class DataLoader implements CommandLineRunner {
         user2.getIssuesCreated().add(issue2);
         user2.getIssuesObserve().add(issue2);
 
+        User user3 = User.builder().userName("user3").password(passwordConfig.passwordEncoder().encode("user3")).role(UserRoles.TEAM_LEADER).build();
+
         User savedUser1 = userService.save(user1);
         User savedUser2 = userService.save(user2);
+        User savedUser3 = userService.save(user3);
 
         savedUser1.getIssuesObserve().add(issue2);
 
@@ -57,16 +60,24 @@ public class DataLoader implements CommandLineRunner {
         savedUser1.getCommentsCreated().add(comment4);
 
         Team team = Team.builder().teamName("team 1").build();
+        Team team2 = Team.builder().teamName("team 2").build();
 
         Team savedTeam = teamService.save(team);
+        Team savedTeam2 = teamService.save(team2);
 
         savedTeam.setLeader(savedUser1);
         savedTeam.setMembers(Set.of(savedUser1, savedUser2));
+
+        savedTeam2.setLeader(savedUser3);
+        savedTeam2.setMembers(Set.of(savedUser3));
 
         savedUser1.setLeaderOfTeam(savedTeam);
         savedUser1.setMemberOfTeam(savedTeam);
 
         savedUser2.setMemberOfTeam(savedTeam);
+
+        savedUser3.setLeaderOfTeam(savedTeam2);
+        savedUser3.setMemberOfTeam(savedTeam2);
 
         Project project = Project.builder()
                 .projectName("project 1")
@@ -79,8 +90,13 @@ public class DataLoader implements CommandLineRunner {
         savedUser1.getIssuesCreated().get(0).setProject(project);
         savedUser2.getIssuesCreated().get(0).setProject(project);
 
+        Project project2 = Project.builder().projectName("project 2").assignedTeam(savedTeam2).build();
+
         savedTeam.setProjects(Set.of(project));
 
+        savedTeam2.setProjects(Set.of(project2));
+
         teamService.save(savedTeam);
+        teamService.save(savedTeam2);
     }
 }
