72c02a9f8dcc5ea6e62d450c22308090718a2ec2
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-05-20 00:14:22 +0200

    added annotation mocking user with role team leader #259

---
+++ issue-tracker-web/src/test/java/com/bozo/issuetracker/controllers/security/annotation/WithMockUserRoleTeamLeader.java
@@ -0,0 +1,14 @@
+package com.bozo.issuetracker.controllers.security.annotation;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(roles = "TEAM_LEADER")
+public @interface WithMockUserRoleTeamLeader {
+}
