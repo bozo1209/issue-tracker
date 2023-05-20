4da8483d31d5b94a8825b5142f8c4033a8387587
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-05-20 23:33:14 +0200

    refactor security test index controller #260

---
+++ issue-tracker-web/src/test/java/com/bozo/issuetracker/controllers/security/indexSecurityController/IndexControllerSecurityAdminTest.java
@@ -0,0 +1,34 @@
+package com.bozo.issuetracker.controllers.security.indexSecurityController;
+
+import com.bozo.issuetracker.controllers.IndexController;
+import com.bozo.issuetracker.controllers.config.ApplicationSecurityTestConfig;
+import com.bozo.issuetracker.controllers.security.annotation.WithMockUserRoleAdmin;
+import com.bozo.issuetracker.details.service.ApplicationUserDetailsService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(ApplicationSecurityTestConfig.class)
+@WebMvcTest(controllers = IndexController.class)
+public class IndexControllerSecurityAdminTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ApplicationUserDetailsService applicationUserDetailsService;
+
+    @WithMockUserRoleAdmin
+    @Test
+    public void indexPageAdmin() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+
+}
