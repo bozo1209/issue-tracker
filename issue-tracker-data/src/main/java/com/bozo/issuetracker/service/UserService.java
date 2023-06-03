26b012c11ad6cba0c5d2872de8119dc912f25c65
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-06-03 13:13:32 +0200

    add method in userservice interface for looking users without team #252

--- issue-tracker-data/src/main/java/com/bozo/issuetracker/service/UserService.java
+++ issue-tracker-data/src/main/java/com/bozo/issuetracker/service/UserService.java
@@ -2,9 +2,13 @@ package com.bozo.issuetracker.service;
 
 import com.bozo.issuetracker.model.User;
 
+import java.util.List;
+
 public interface UserService extends CrudService<User, Long>{
 
     User findByUserName(String name);
+
+    List<User> findByMemberOfTeamIsNull();
 
     void updateUserInCache(User user);
 }
