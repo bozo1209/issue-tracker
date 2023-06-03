d9c2276582a52e38569a13b93b6cdb3c793144a1
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-06-03 13:12:21 +0200

    add repository method for looking users without team #252

--- issue-tracker-data/src/main/java/com/bozo/issuetracker/repository/UserRepository.java
+++ issue-tracker-data/src/main/java/com/bozo/issuetracker/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.bozo.issuetracker.repository;
 import com.bozo.issuetracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserName(String userName);
+
+    List<User> findByMemberOfTeamIsNull();
 }
